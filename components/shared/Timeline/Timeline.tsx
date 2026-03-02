c3b3550f6bd58e9a583b9f8d77840d0c9db4230e
Christian Leonard <thamrchris@gmail.com>
2026-03-02 18:09:17 +0700

    style: add log date

--- components/shared/Timeline/Timeline.tsx
+++ components/shared/Timeline/Timeline.tsx
@@ -3,6 +3,7 @@
 import React, { FC } from 'react'
 import { handleTransactionStatus } from '@/lib/statuses'
 import { LogProps } from '@/type/Logs'
+import { formattedDate } from '@/lib/utils'
 
 interface TimelineProps {
   logs?: LogProps[]
@@ -13,23 +14,28 @@ const Timeline: FC<TimelineProps> = ({ logs }) => {
   // TODO: add loading skeleton while log is loading
 
   return (
-    <div className="w-full flex flex-col gap-4">
+    <div className="w-full flex flex-col gap-6">
       {logs?.map((log, idx) => (
         <div key={idx} className="flex gap-5 justify-start">
           <div className="w-fit flex flex-col items-center gap-4">
             <div className="size-3 shrink-0 rounded-full bg-foreground outline outline-offset-3" />
             {idx !== logs.length - 1 && (
-              <div className="h-full min-h-15 w-[0.085rem] bg-foreground rounded-full" />
+              <div className="h-full min-h-24 w-[0.085rem] bg-foreground rounded-full" />
             )}
           </div>
-          <div className="flex flex-col gap-1 -mt-2">
-            <div>{handleTransactionStatus(log.status)}</div>
-            <span className="font-medium text-[0.75rem] text-muted-foreground ml-2">
-              Notes: {log.notes}
+          <div className="flex flex-col gap-3 -mt-1">
+            <span className="font-semibold text-[0.75rem] text-foreground/90">
+              {formattedDate(log.log_date)}
             </span>
-            <span className="font-semibold text-[0.75rem] text-foreground/90 ml-2">
-              By: {log.user_name}
-            </span>
+            <div className="flex flex-col gap-1 ml-3">
+              <div>{handleTransactionStatus(log.status)}</div>
+              <span className="font-medium text-[0.75rem] text-muted-foreground ml-2">
+                Notes: {log.notes}
+              </span>
+              <span className="font-semibold text-[0.75rem] text-foreground/90 ml-2">
+                By: {log.user_name}
+              </span>
+            </div>
           </div>
         </div>
       ))}
