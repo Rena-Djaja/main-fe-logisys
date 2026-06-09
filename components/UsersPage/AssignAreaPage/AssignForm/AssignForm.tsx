96fe52697642039647623b29a64a3785bef029ab
Christian Leonard <thamrchris@gmail.com>
2026-06-09 16:51:00 +0700

    feat: disable submit button if no location is selected

--- components/UsersPage/AssignAreaPage/AssignForm/AssignForm.tsx
+++ components/UsersPage/AssignAreaPage/AssignForm/AssignForm.tsx
@@ -93,21 +93,6 @@ const AssignForm: FC<AssignAreaFormProps> = (props) => {
                 )}
               />
             </div>
-            {/*<div>*/}
-            {/*  <CustomButton*/}
-            {/*    label={'Add'}*/}
-            {/*    type={ButtonType.BUTTON}*/}
-            {/*    onClick={handleAddLocation}*/}
-            {/*    disabled={*/}
-            {/*      !form.watch('location_id') ||*/}
-            {/*      !!form.formState.errors?.location_id ||*/}
-            {/*      isLoading.validate ||*/}
-            {/*      fields*/}
-            {/*        .map((each: any) => each.location_id)*/}
-            {/*        .includes(form.watch('location_id'))*/}
-            {/*    }*/}
-            {/*  />*/}
-            {/*</div>*/}
           </div>
           <DialogFooter className="mt-4">
             <DialogClose asChild>
@@ -121,7 +106,7 @@ const AssignForm: FC<AssignAreaFormProps> = (props) => {
             <CustomButton
               type={ButtonType.SUBMIT}
               label={'Simpan'}
-              disabled={isLoading.validate}
+              disabled={isLoading.validate || !form.watch('locations').length}
               isLoading={isLoading.submit}
             />
           </DialogFooter>
