78f23844f26fff1361bda50e462d5da8c8b7e3e6
Christian Leonard <thamrchris@gmail.com>
2026-07-14 17:01:36 +0700

    Merge pull request #53
    * feat: update supplier v2

--- components/SuppliersPage/Form/useSupplierForm.tsx
+++ components/SuppliersPage/Form/useSupplierForm.tsx
@@ -34,6 +34,7 @@ const useSupplierForm = (props: CommonFormProps) => {
   const { id } = props
   const {
     selectedLocation: locDetails,
+    setSelectedLocation: setLocDetails,
     setSelectedLocations,
     setIsSelectButtonLoading,
   } = useMapContext()
@@ -110,13 +111,52 @@ const useSupplierForm = (props: CommonFormProps) => {
   }
 
   const handleSuccessFetchDetails = (response: SupplierDetailsResponse) => {
+    const { data } = response
     const fieldsToUpdate = ['name', 'address', 'phone_number'] as const
+    const dataToUpdate = data as Record<SelectedFields, string>
     type SelectedFields = (typeof fieldsToUpdate)[number]
-    const { data }: { data: Record<SelectedFields, string> } = response
 
     fieldsToUpdate.forEach((each) => {
-      form.setValue(each, data[each] || '')
+      form.setValue(each, dataToUpdate[each] || '')
     })
+
+    const locationData: LocationByLatLngRowProps = {
+      province_id: data.location.province_id,
+      province_name: data.location.province_name,
+      regency_id: data.location.regency_id,
+      regency_name: data.location.regency_name,
+      district_id: data.location.district_id,
+      district_name: data.location.district_name,
+      village_id: data.location.village_id,
+      village_name: data.location.village_name,
+      distance_meters: 0,
+    }
+
+    setSelectedLocation(locationData)
+
+    locationFormKey.forEach((key) => {
+      // @ts-ignore
+      form.clearErrors(key)
+      form.setValue(
+        // @ts-ignore
+        key,
+        locationData[key as keyof LocationByLatLngRowProps]
+      )
+    })
+
+    const location: LocationFeature = {
+      properties: {
+        name: data.name,
+        full_address: data.address,
+        mapbox_id: data.id,
+        feature_type: 'supplier-location',
+        coordinates: {
+          latitude: data.location.latitude,
+          longitude: data.location.longitude,
+        },
+      },
+    }
+    setLocDetails(location)
   }
 
   const handleFailureFetchDetails = (response?: SupplierDetailsResponse) => {
@@ -173,7 +213,7 @@ const useSupplierForm = (props: CommonFormProps) => {
         SupplierAPI.POST_SUPPLIER,
         {
           ...data,
-          ...(id && { id: Number(id) }),
+          ...(id && { id }),
           phone_number: data.phone_number || null,
         },
         { method: id ? 'PUT' : 'POST' }
